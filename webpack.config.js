cc771fc69a37d5f5d5cf0fd15158fe8a6b8e4765
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-02-27 15:32:38 +0000

    output asset directories

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[hash][ext][query]",
+        },
+      },
     ],
   },
   plugins: [
