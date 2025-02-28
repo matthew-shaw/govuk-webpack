e8c8484f00d65641c0e59f532964afa6b8530f07
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-02-28 00:14:50 +0000

    set up babel

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
         ],
       },
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
